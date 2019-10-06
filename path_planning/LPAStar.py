8b9e6b8c6f469cf335d831c0705ac7e24cf6b542
Nanda-Kishore-V <blazingkishore@gmail.com>
2019-10-05 23:19:27 -0400

    Add Lifelong Planning A*
    This commit also has the code to save images as gif in scripts/lpa_star.py

---
+++ path_planning/LPAStar.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+import numpy as np
+import itertools
+import operator
+import heapq
+
+# TODO: When priority queue is empty return with path not found
+class LPAStar:
+    def __init__(self, grid, start, goal):
+        self.grid = grid
+        dim = len(grid.shape)
+        self.directions = [p for p in itertools.product([-1, 0, 1], repeat=dim) if any(p) != 0]
+        self.direction_cost = []
+        for i in range(len(self.directions)):
+            cost = sum(tuple(map(operator.mul, self.directions[i], self.directions[i])))
+            self.direction_cost.append(cost)
+        self.path = None
+        self.reset(start, goal)
+
+    def reset(self, start, goal):
+        self.start = start
+        self.goal = goal
+        self.g = {}
+        self.rhs = {}
+        self.path = {}
+        self.initialize()
+        self.compute_shortest_path()
+
+    @staticmethod
+    def distance(location_1, location_2):
+        distance = 0
+        for i in range(len(location_1)):
+            distance += (location_1[i] - location_2[i])**2
+        return np.sqrt(distance)
+
+    def heuristic(self, location):
+        return self.distance(location, self.goal)
+
+    def check_location(self, location):
+        for i in range(len(location)):
+            if location[i] <0 or location[i] >= self.grid.shape[i]:
+                return False
+        return True
+
+    def check_validity(self, location):
+        return self.check_location(location)
+        if self.grid[location] != 0.0:
+            return False
+        return True
+
+    def initialize(self):
+        self.queue = []
+        self.rhs[self.start] = 0
+        heapq.heappush(self.queue, (self.calculate_key(self.start), self.start))
+
+    def calculate_key(self, node):
+        k2 = min(self.g.get(node, np.inf), self.rhs.get(node, np.inf))
+        return [k2 + self.heuristic(node), k2]
+
+    def update_vertex(self, node):
+        if self.check_location(node):
+            if self.grid[node] == 1:
+                self.g[node] = np.inf
+                self.rhs[node] = np.inf
+                return
+        if node != self.start:
+            min_value = np.inf
+            for idx, direction in enumerate(self.directions):
+                neighbor = tuple(map(operator.add, node, direction))
+                if self.check_location(neighbor):
+                    if self.grid[neighbor] != 0:
+                        current_value = np.inf
+                    else:
+                        current_value = self.g.get(neighbor, np.inf) + self.direction_cost[idx]
+                    if min_value > current_value:
+                        min_value = current_value
+            self.rhs[node] = min_value
+        self.queue = [i for i in self.queue if i[1] != node]
+        heapq.heapify(self.queue)
+        if self.g.get(node, np.inf) != self.rhs.get(node, np.inf):
+            heapq.heappush(self.queue, (self.calculate_key(node), node))
+
+    def compute_shortest_path(self):
+        while heapq.nsmallest(1, self.queue)[0][0] < self.calculate_key(self.goal) or \
+                self.rhs.get(self.goal, np.inf) != self.g.get(self.goal, np.inf):
+            k, current_node = heapq.heappop(self.queue)
+            if self.g.get(current_node, np.inf) > self.rhs.get(current_node, np.inf):
+                self.g[current_node] = self.rhs[current_node]
+                for direction in self.directions:
+                    neighbor = tuple(map(operator.add, current_node, direction))
+                    if self.check_validity(neighbor):
+                        self.update_vertex(neighbor)
+            else:
+                self.g[current_node] = np.inf
+                for direction in self.directions:
+                    neighbor = tuple(map(operator.add, current_node, direction))
+                    if self.check_validity(neighbor):
+                        self.update_vertex(neighbor)
+                self.update_vertex(current_node)
+
+    def generate_path(self, grid):
+        diff_grid = self.grid - grid
+        self.grid = grid
+        I, J = np.nonzero(diff_grid)
+        for i, j in zip(I, J):
+            self.g[(i, j)] = np.inf
+            self.rhs[(i, j)] = np.inf
+
+            for direction in self.directions:
+                neighbor = tuple(map(operator.add, (i, j), direction))
+                if self.check_location(neighbor):
+                    if self.grid[neighbor] != 0:
+                        self.g[neighbor] = np.inf
+                        self.rhs[neighbor] = np.inf
+                    else:
+                        self.update_vertex(neighbor)
+
+        self.compute_shortest_path()
+
+    def retrieve_path(self):
+        self.path = [self.goal]
+        node = self.goal
+        while node != self.start:
+            min_rhs = self.rhs.get(node, np.inf)
+            min_neighbor = None
+            for direction in self.directions:
+                neighbor = tuple(map(operator.add, node, direction))
+                if self.rhs.get(neighbor, np.inf) < min_rhs:
+                    min_rhs = self.rhs.get(neighbor, np.inf)
+                    min_neighbor = neighbor
+            if min_neighbor is None:
+                print("Failed!")
+                return -1
+            self.path.append(min_neighbor)
+            node = min_neighbor
+        return self.path[::-1]
---
+++ scripts/lpa_star.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+import os
+import sys
+import numpy as np
+from matplotlib import pyplot as plt
+
+sys.path.append('../')
+from path_planning.LPAStar import LPAStar
+
+from tempfile import mkstemp, mkdtemp
+from imageio import imread, mimsave
+
+def main():
+    grid = np.zeros((80, 100))
+    start = (2, 17)
+    goal = (62, 75)
+    path_planner = LPAStar(grid.copy(), start, goal)
+    path = path_planner.retrieve_path()
+
+    obstacle_groups = []
+    obstacles = []
+    for i in range(5, 20):
+        for j in range(10, 30):
+            obstacles.append((i, j))
+    obstacle_groups.append(obstacles)
+
+    obstacles = []
+    for i in range(40, 60):
+        for j in range(10, 80):
+            obstacles.append((i, j))
+    obstacle_groups.append(obstacles)
+
+    obstacles = []
+    for i in range(30, 35):
+        for j in range(70, 90):
+            obstacles.append((i, j))
+    obstacle_groups.append(obstacles)
+
+    obstacles = []
+    for i in range(35, 40):
+        for j in range(75, 80):
+            obstacles.append((i, j))
+    obstacle_groups.append(obstacles)
+
+    fig = plt.figure(1)
+    # Plot boundaries
+    plt.plot([-1]*(grid.shape[1]+2), range(-1, grid.shape[1]+1, 1), '.k')
+    plt.plot(range(-1, grid.shape[0]+1, 1), [-1]*(grid.shape[0]+2), '.k')
+    plt.plot([grid.shape[0]]*(grid.shape[1]+2), range(-1, grid.shape[1]+1, 1), '.k')
+    plt.plot(range(-1, grid.shape[0]+1, 1), [grid.shape[1]+1]*(grid.shape[0]+2), '.k')
+
+    for p in path:
+        plt.plot(p[0], p[1], '.r')
+    plt.plot(start[0], start[1], '.b')
+    plt.plot(goal[0], goal[1], '.g')
+
+    plt.axis('equal')
+    plt.title("Dynamic Replanning using Lifelong Planning A*")
+
+    tempdir = mkdtemp()
+    image_name = []
+
+    _, filename = mkstemp(dir=tempdir)
+    filename += '.png'
+    fig.savefig(filename)
+    image_name.append(filename)
+
+    plt.pause(1)
+
+    for idx, obstacle in enumerate(obstacle_groups):
+        for obs in obstacle:
+            grid[obs] = 1.0
+
+        path_planner.generate_path(grid.copy())
+        path = path_planner.retrieve_path()
+
+        plt.cla()
+
+        # Plot boundaries
+        plt.plot([-1]*(grid.shape[1]+2), range(-1, grid.shape[1]+1, 1), '.k')
+        plt.plot(range(-1, grid.shape[0]+1, 1), [-1]*(grid.shape[0]+2), '.k')
+        plt.plot([grid.shape[0]]*(grid.shape[1]+2), range(-1, grid.shape[1]+1, 1), '.k')
+        plt.plot(range(-1, grid.shape[0]+1, 1), [grid.shape[1]+1]*(grid.shape[0]+2), '.k')
+
+        # Plot obstacles
+        for i in range(idx+1):
+            for obs in obstacle_groups[i]:
+                plt.plot(obs[0], obs[1], '.k')
+
+        for p in path:
+            plt.plot(p[0], p[1], '.r')
+        plt.plot(start[0], start[1], '.b')
+        plt.plot(goal[0], goal[1], '.g')
+
+        plt.axis('equal')
+        fig.savefig(filename)
+
+        _, filename = mkstemp(dir=tempdir)
+        filename += '.png'
+        fig.savefig(filename)
+        image_name.append(filename)
+
+        plt.pause(1)
+
+    images = []
+    for png in image_name:
+        img = imread(png)
+        images.append(img)
+    mimsave('LPA_star.gif', images, duration=1)
+
+def main2():
+    grid = np.zeros((5, 5))
+    grid[1, 2] = 1
+    grid[1, 3] = 1
+    grid[2, 1] = 1
+
+    start = (0, 4)
+    goal = (3, 1)
+
+    path_planner = LPAStar(grid, start, goal)
+
+    data_g = np.zeros((5, 5))
+    data_rhs = np.zeros((5, 5))
+    for i in range(5):
+        for j in range(5):
+           data_g[i, j] = path_planner.g.get((i, j), np.inf)
+           data_rhs[i, j] = path_planner.rhs.get((i, j), np.inf)
+
+    print("G:")
+    print(data_g)
+    print("RHS:")
+    print(data_rhs)
+
+    grid_copy = grid.copy()
+    grid_copy[1, 4] = 1
+    path_planner.generate_path(grid_copy)
+
+    data_g = np.zeros((5, 5))
+    data_rhs = np.zeros((5, 5))
+    for i in range(5):
+        for j in range(5):
+           data_g[i, j] = path_planner.g.get((i, j), np.inf)
+           data_rhs[i, j] = path_planner.rhs.get((i, j), np.inf)
+
+    print("G:")
+    print(data_g)
+    print("RHS:")
+    print(data_rhs)
+
+if __name__=="__main__":
+    main()
+    # main2()
